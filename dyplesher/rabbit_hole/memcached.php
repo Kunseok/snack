b1520643968895190e843f07d55955c206362d15
blee <tastypie@gmail.com>
2020-08-03 11:56:15 -0400

    reorg

--- dyplesher/rabbit_hole/memcached.php
+++
@@ -1,8 +0,0 @@
-<?php
-// initiate the memcached instance
-$m = new Memcached();
-$m->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
-$m->setSaslAuthData("felamos", "zxcvbnm");
-$m->addServer('10.10.10.190', 11211);
-print_r( $m->getOption() );
-?>
